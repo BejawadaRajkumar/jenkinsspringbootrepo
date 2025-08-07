3f35024049442043a5b5e8fcca923819d84be0b8
Raj <bejawadarajkumar282@gmail.com>
2025-08-07 21:32:45 +0530

    spring boot project v1.0

---
+++ src/main/java/com/klef/CilentController.java
@@ -0,0 +1,30 @@
+package com.klef;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CilentController {
+	 @GetMapping("/")
+      public String home() {
+    	  return "CI/CD and Cloud DevOps";
+      }
+	 @GetMapping("/demo")
+     public String demo() {
+   	  return "Jenkins Demo";
+     }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
